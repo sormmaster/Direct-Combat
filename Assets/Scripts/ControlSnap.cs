772d985238b140e689898311e011afe77b3d23d0
sormmaster <34348746+sormmaster@users.noreply.github.com>
2020-08-16 11:31:32 -0500

    Grid Logic Started

---
+++ Assets/Scripts/ControlSnap.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ControlSnap : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] [Range(0f, 10f)] float forcedHeight = 0f;
+    [SerializeField] [Range(1f, 10f)] float trueScale = 5f;
+    void Update()
+    {
+        updatePosition();
+
+        updateScale();
+       
+    }
+
+    void updatePosition()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / trueScale) * trueScale;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / trueScale) * trueScale;
+        snapPos.y = forcedHeight;
+        transform.position = snapPos;
+    }
+
+    void updateScale()
+    {
+        if (transform.localScale.x != trueScale)
+        {
+            transform.localScale = new Vector3(trueScale, trueScale, trueScale);
+
+        }
+    }
+}
