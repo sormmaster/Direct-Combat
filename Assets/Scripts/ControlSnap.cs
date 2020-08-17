d82202c57e97f9fbb224caaca0fb8e35b16facb6
sormmaster <34348746+sormmaster@users.noreply.github.com>
2020-08-17 12:19:20 -0500

    Added Labels to Cubes

--- Assets/Scripts/ControlSnap.cs
+++ Assets/Scripts/ControlSnap.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [ExecuteInEditMode]
 public class ControlSnap : MonoBehaviour
@@ -9,21 +10,36 @@ public class ControlSnap : MonoBehaviour
 
     [SerializeField] [Range(0f, 10f)] float forcedHeight = 0f;
     [SerializeField] [Range(1f, 10f)] float trueScale = 5f;
+    Vector3 lastTransform = new Vector3();
+
+    TextMesh textMesh;
+    void Start()
+    {
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = "TEST";
+    }
     void Update()
     {
         updatePosition();
-
         updateScale();
        
     }
 
     void updatePosition()
     {
-        Vector3 snapPos;
-        snapPos.x = Mathf.RoundToInt(transform.position.x / trueScale) * trueScale;
-        snapPos.z = Mathf.RoundToInt(transform.position.z / trueScale) * trueScale;
-        snapPos.y = forcedHeight;
-        transform.position = snapPos;
+        if (!transform.Equals(lastTransform))
+        {
+            Vector3 snapPos;
+            snapPos.x = Mathf.RoundToInt(transform.position.x / trueScale) * trueScale;
+            snapPos.z = Mathf.RoundToInt(transform.position.z / trueScale) * trueScale;
+            snapPos.y = forcedHeight;
+            transform.position = snapPos;
+            lastTransform = transform.position;
+            if(textMesh)
+            {
+                textMesh.text = snapPos.x + "x" + snapPos.z;
+            }
+        }
     }
 
     void updateScale()
